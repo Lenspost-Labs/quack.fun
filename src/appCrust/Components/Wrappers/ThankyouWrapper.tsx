6876dc208fbed96e85f94a5dee78c76b9e74b055
Scriptscrypt <tirumaladasa@gmail.com>
2024-02-15 21:11:22 +0530

    Farcaster server error #2

--- src/appCrust/Components/Wrappers/ThankyouWrapper.tsx
+++ src/appCrust/Components/Wrappers/ThankyouWrapper.tsx
@@ -1,10 +1,22 @@
 import React from "react";
+import Lottie from "react-lottie";
+import animationData from "../../../assets/Animations/Lottie/login/solanaAnimation.json";
 
 const ThankyouWrapper = () => {
   return (
     <>
-      <div>Thank you for your overwhelming support</div>
-      <div className=""> Farcaster is facing some issues, we'll be back soon</div>
+   
+      <div className="flex flex-col align-middle justify-center items-center m-8">
+      QUACK QUACK!
+        <Lottie height={200} options={{ animationData: animationData }} />
+        <div>Thankyou for your overwhelming response</div>
+        <div className="mt-4">
+          Farcaster server is currently dreaming of faster processors and more
+          RAM. While it's in dreamland, our team is working hard to get it back
+          to reality. We appreciate your patience and promise to be back faster
+          than you can say 'server reboot'!
+        </div>
+      </div>
     </>
   );
 };
