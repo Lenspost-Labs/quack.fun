dbed00750f1774cce099b9880d245b0439f723a7
Scriptscrypt <tirumaladasa@gmail.com>
2024-02-15 18:06:02 +0530

    Login Flow Fix #2

--- src/appCrust/Components/Items/SolLoginBtn.tsx
+++ src/appCrust/Components/Items/SolLoginBtn.tsx
@@ -107,7 +107,7 @@ const SolLoginBtnUI: FC = () => {
 
   return (
     <>
-      {!connected && (
+      {!connected && !hasUserLoggedIn && (
         <>
           <WalletMultiButton>
             <div className="hidden md:flex justify-center">Select Wallet</div>
@@ -115,7 +115,7 @@ const SolLoginBtnUI: FC = () => {
           </WalletMultiButton>
         </>
       )}
-      {connected && (
+      {connected && hasUserLoggedIn && (
         <>
           <div className="flex justify-between items-center align-middle ">
             <Link
